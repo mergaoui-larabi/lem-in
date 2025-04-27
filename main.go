0110e7ea9114da96385a80b1ed1b8c2a3615d841
Your Name <you@example.com>
2025-04-27 17:31:08 +0100

    test files uploaded

--- main.go
+++ main.go
@@ -2,25 +2,32 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"lem-in/graph"
 	extract "lem-in/parse"
-	"lem-in/solver"
 )
 
 func main() {
+	start := time.Now()
 	var coords []graph.Room
 
 	newGraph := graph.Graph{Colony: make(map[string][]string)}
-	err := extract.Parse("./tests/example00.txt", &newGraph, &coords)
+	err := extract.Parse("./tests/maps/audit/badexample00", &newGraph, &coords)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	fmt.Println(newGraph.Colony)
 
-	paths := solver.FindPaths(newGraph.Colony, newGraph.Start.Name, newGraph.End.Name)
-	fmt.Println(paths)
-	
-	solver.Solver(&newGraph)
+	for i, r := range newGraph.Colony {
+
+		fmt.Println(i, r)
+	}
+
+	// paths := solver.FindPaths(newGraph.Colony, newGraph.Start.Name, newGraph.End.Name)
+	// fmt.Println(paths)
+
+	// solver.Solver(&newGraph)
+
+	fmt.Println("THIS EXEC TOOK", time.Since(start))
 }
